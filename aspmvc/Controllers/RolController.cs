6bfeaf3abeaa2177bc8c027655ff069d84e3aec4
fernaCode-programing <zbelfx@icloud.com>
2024-06-10 17:34:45 -0400

    migracion de modelos, clases y controladores

---
+++ aspmvc/Controllers/RolController.cs
@@ -0,0 +1,83 @@
+﻿using aspmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspmvc.Controllers
+{
+    public class RolController : Controller
+    {
+        // GET: Rol
+        public ActionResult Index()
+        {
+            List<RolCLS> listaRol = new List<RolCLS>();
+            using (var bd = new BDPasajeEntities())
+            {
+                listaRol = (from rol in bd.Rol
+                            where rol.BHABILITADO == 1
+                            select new RolCLS
+                            {
+                                iidRol = rol.IIDROL,
+                                nombre = rol.NOMBRE,
+                                descripcion = rol.DESCRIPCION
+
+                            }).ToList();
+            }
+            return View(listaRol);
+        }
+
+        public ActionResult Filtro(string nombre)
+        {
+
+            List<RolCLS> listaRol = new List<RolCLS>();
+            using (var bd = new BDPasajeEntities())
+            {
+                if (nombre == null)
+                {
+                    listaRol = (from rol in bd.Rol
+                                where rol.BHABILITADO == 1
+                                select new RolCLS
+                                {
+                                    iidRol = rol.IIDROL,
+                                    nombre = rol.NOMBRE,
+                                    descripcion = rol.DESCRIPCION
+
+                                }).ToList();
+                }
+                else
+                    listaRol = (from rol in bd.Rol
+                                where rol.BHABILITADO == 1
+                                && rol.NOMBRE.Contains(nombre)
+                                select new RolCLS
+                                {
+                                    iidRol = rol.IIDROL,
+                                    nombre = rol.NOMBRE,
+                                    descripcion = rol.DESCRIPCION
+
+                                }).ToList();
+
+            }
+            return PartialView("_TablaRol", listaRol);
+        }
+        public int Guardar(RolCLS oRolCLS, int titulo)
+        {
+            int rpta = 0;
+            using (var bd = new BDPasajeEntities())
+            {
+                if (titulo.Equals(1))
+                {
+                    Rol oRol = new Rol();
+                    oRol.NOMBRE = oRolCLS.nombre;
+                    oRol.DESCRIPCION = oRolCLS.descripcion;
+                    oRol.BHABILITADO = 1;
+                    bd.Rol.Add(oRol);
+                    rpta = bd.SaveChanges();
+                }
+            }
+            return rpta;
+        }
+    
+    }
+}
---
+++ aspmvc/Controllers/TipoUsuarioController.cs
@@ -0,0 +1,54 @@
+﻿using aspmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspmvc.Controllers
+{
+    public class TipoUsuarioController : Controller
+    {
+        // GET: TipoUsuario
+        private TipoUsuarioCLS oTipoVal;
+        private bool buscarTipoUser(TipoUsuarioCLS oTipoUser)
+        {
+            bool busquedaId = true;
+            bool busdaNom = true;
+            bool busquedaDescrip = true;
+            if (oTipoVal.iidTipoUsuario > 0)
+                busquedaId = oTipoUser.iidTipoUsuario.ToString().Contains(oTipoVal.iidTipoUsuario.ToString());
+            if (oTipoVal.nombre != null)
+                busdaNom = oTipoUser.nombre.ToString().Contains(oTipoVal.nombre);
+            if (oTipoVal.descripcion != null)
+                busquedaDescrip = oTipoUser.descripcion.ToString().Contains(oTipoVal.descripcion);
+            return (busquedaId && busdaNom && busquedaDescrip);
+        }
+        public ActionResult Index(TipoUsuarioCLS oTipoUser)
+        {
+            oTipoVal = oTipoUser;
+            List<TipoUsuarioCLS> listaTipoUsuario = null;
+            //pongo variable
+            List<TipoUsuarioCLS> listaFiltrado;
+            using (var bd = new BDPasajeEntities())
+            {
+                listaTipoUsuario = (from tipousuario in bd.TipoUsuario
+                                    where tipousuario.BHABILITADO == 1
+                                    select new TipoUsuarioCLS
+                                    {
+                                        iidTipoUsuario = tipousuario.IIDTIPOUSUARIO,
+                                        nombre = tipousuario.NOMBRE,
+                                        descripcion = tipousuario.DESCRIPCION
+                                    }).ToList();
+                if (oTipoUser.iidTipoUsuario == 0 && oTipoUser.nombre == null && oTipoUser.descripcion == null) listaFiltrado = listaTipoUsuario;
+                else
+                {
+                    Predicate<TipoUsuarioCLS> pred = new Predicate<TipoUsuarioCLS>(buscarTipoUser);
+                    listaFiltrado = listaTipoUsuario.FindAll(pred);
+                }
+
+            }
+            return View(listaFiltrado);
+        }
+    }
+}
